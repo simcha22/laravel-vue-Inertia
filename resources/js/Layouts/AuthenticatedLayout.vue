82ebef887a79ce9a04cc2a7a11917df7ea29ee3d
simcha fridman <simchafridman@simchas-MacBook-Air.local>
2024-07-07 18:09:08 +0300

    add post index

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,6 +32,9 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('posts.index')" :active="route().current('posts.index')">
+                                    Blog Post
+                                </NavLink>
                             </div>
                         </div>
 
---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+    import {Head} from "@inertiajs/vue3";
+
+    const props = defineProps(['posts'])
+
+    console.log(props.posts)
+</script>
+
+<template>
+    <Head title="Posts" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Posts</h2>
+        </template>
+
+
+    </AuthenticatedLayout>
+
+</template>
