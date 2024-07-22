10991c9d291e57066dc611e74528ea56df34a4fd
simcha fridman <simchafridman@simchas-MacBook-Air.local>
2024-07-22 22:26:52 +0300

    select style

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -81,7 +81,7 @@ const savePost = () =>{
                             <div>
                                 <InputLabel for="role" value="Role" />
 
-                                <Select v-model="form.role" :items="roles"/>
+                                <Select class="mt-1 block w-full" v-model="form.role" :items="roles"/>
 
                                 <InputError class="mt-2" :message="form.errors.role" />
                             </div>
