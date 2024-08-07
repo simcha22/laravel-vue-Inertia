a9e901f757b94412008db3fce7703ec60e7a5d20
simcha fridman <simchafridman@simchas-MacBook-Air.local>
2024-08-07 16:36:24 +0300

    delete result

--- resources/js/Pages/Results/Index.vue
+++ resources/js/Pages/Results/Index.vue
@@ -80,10 +80,11 @@
                                     <!--                                          class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">-->
                                     <!--                                        Edit-->
                                     <!--                                    </Link>-->
-                                    <!--                                    <button v-if="can('user.delete')" type="button" @click="confirmingDeleteUser(user.id)"-->
-                                    <!--                                            class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">-->
-                                    <!--                                        Delete-->
-                                    <!--                                    </button>-->
+                                    <button type="button"
+                                            @click="confirmingDeleteResult(result.id)"
+                                            class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                                        Delete
+                                    </button>
                                 </td>
                             </tr>
                             </tbody>
@@ -93,22 +94,57 @@
                 </div>
             </div>
         </div>
-        <PsrModal :show="showAddResults" @close="closeModel" :exercise="exercise"/>
+        <PsrModal :show="showAddResults" @close="closeModal" :exercise="exercise"/>
+
+        <Modal :show="confirmingResultDeletion" @close="closeModal">
+            <div class="p-6">
+                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                    Are you sure you want to delete this log?
+                </h2>
+
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click="closeModal"> Cancel</SecondaryButton>
+
+                    <DangerButton
+                        class="ms-3"
+                        @click="deleteResult"
+                    >
+                        Delete
+                    </DangerButton>
+                </div>
+            </div>
+        </Modal>
     </AuthenticatedLayout>
 </template>
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import {Head, Link} from "@inertiajs/vue3";
+import {Head, Link, router} from "@inertiajs/vue3";
 import {ref} from "vue";
 import PsrModal from "@/Pages/Results/Components/PsrModal.vue";
 import StatisticsCard from "@/Pages/Results/Components/StatisticsCard.vue";
 import useTime from "@/Composteble/time.js";
+import Modal from "@/Components/Modal.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import DangerButton from "@/Components/DangerButton.vue";
 
 const {formatMessageDateLong} = useTime()
 const props = defineProps(['exercise', 'cards'])
 const showAddResults = ref(false)
+const resultToDelete = ref(null)
+const confirmingResultDeletion = ref(false)
 
-const closeModel = () => {
+const confirmingDeleteResult = (id) => {
+    resultToDelete.value = id
+    confirmingResultDeletion.value = true
+}
+
+const deleteResult = () => {
+    router.delete(route('results.destroy', {exercise: props.exercise[0].id, result: resultToDelete.value}))
+    closeModal()
+}
+
+const closeModal = () => {
+    confirmingResultDeletion.value = false
     showAddResults.value = false
 }
 
